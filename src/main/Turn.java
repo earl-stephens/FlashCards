8264dd197db60c3b58f22c89f2aa21f3769b5fb4
earl-stephens <stephensearl@hotmail.com>
2023-01-13 16:36:14 -0700

    Test and code for having a guess and a correct method

--- src/main/Turn.java
+++ src/main/Turn.java
@@ -1,8 +1,17 @@
 package main;
 
 public class Turn {
+	public String guess;
+	public Card card;
+	
 	public Turn(String guess, Card card) {
-		
+		this.guess = guess;
+		this.card = card;
 	}
+	
+	public boolean correct() {
+		return guess == card.answer;
+	}
+	
 
 }
--- src/test/TurnTest.java
+++ src/test/TurnTest.java
@@ -16,5 +16,22 @@ class TurnTest {
 		
 		Assert.assertNotNull(turn);
 	}
+	
+	@Test
+	 void shouldHaveAGuessMethod() {
+		String guess = "Juneau";
+		Card card = new Card("What is the capital of Alaska?", "Juneau", "Geography");
+		Turn turn = new Turn(guess, card);
+		
+		Assert.assertEquals(turn.card.answer, guess);
+	}
 
+	@Test
+	 void shouldHaveACorrectMethod() {
+		String guess = "Juneau";
+		Card card = new Card("What is the capital of Alaska?", "Juneau", "Geography");
+		Turn turn = new Turn(guess, card);
+		
+		Assert.assertEquals(true, turn.correct());
+	}
 }
