23cf2e229c78362d65b3bc19d7748eab66ce666d
earl-stephens <stephensearl@hotmail.com>
2023-01-15 14:11:11 -0700

    Refactor to replace Array with ArrayList

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,13 +1,27 @@
 package main;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Deck {
-	public Card card[];
+	public ArrayList<Card> card = new ArrayList<>();
 	
-	public Deck(Card[] card) {
+	public Deck(ArrayList<Card> card) {
 		this.card = card;
 	}
 	
 	public int count() {
-		return card.length;
+		return card.size();
 	}
+	
+	/*public Card cards_in_category(String selectedCategory) {
+		Card categoryArray[];
+		
+		for(int i = 0; i < card.length; i++) {
+			if(card[i].category == selectedCategory) {
+				categoryArray;
+			}
+		}
+		return categoryArray;
+	}
+	*/
 }
--- src/test/DeckTest.java
+++ src/test/DeckTest.java
@@ -2,6 +2,8 @@ package test;
 import main.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List; 
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -10,10 +12,13 @@ class DeckTest {
 	Card card_1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
 	Card card_2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
 	Card card_3 = new Card("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", "STEM");
-	Card cards[] = {card_1, card_2, card_3};
+	ArrayList<Card> cards = new ArrayList<>();
 
 	@Test
 	void testForADeckClass() {
+		cards.add(card_1);
+		cards.add(card_2);
+		cards.add(card_3);
 		Deck deck = new Deck(cards);
 		Assert.assertNotNull(deck);;
 	}
@@ -21,6 +26,9 @@ class DeckTest {
 	@Test
 	void testForNumberOfCardsInADeck() {
 		Deck deck = new Deck(cards);
+		cards.add(card_1);
+		cards.add(card_2);
+		cards.add(card_3);
 		
 		Assert.assertEquals(3, deck.count());
 	}
@@ -28,8 +36,19 @@ class DeckTest {
 	@Test
 	void testForNumberOfCardsInADeckWrongAnswer() {
 		Deck deck = new Deck(cards);
+		cards.add(card_1);
+		cards.add(card_2);
+		cards.add(card_3);
 		
 		Assert.assertNotEquals(5, deck.count());
 	}
-
+	/*
+	@Test
+	void testForNumberOfCardsInACategory() {
+		Deck deck = new Deck(cards);
+		Card categoryArray[] = {card_2, card_3};
+		
+		Assert.assertEquals(categoryArray, deck.cards_in_category("STEM"));
+	}
+	*/
 }
